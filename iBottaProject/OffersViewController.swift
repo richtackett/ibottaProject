bb9ba6d52cd9dae580f3aea788a10e96cd9afb4c
Richard Tackett <richtackett@hotmail.com>
2018-01-23 10:01:22 -0600

    Start of setting up collection view layout

--- iBottaProject/OffersViewController.swift
+++ iBottaProject/OffersViewController.swift
@@ -9,6 +9,26 @@
 import UIKit
 
 class OffersViewController: UIViewController {
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    override func loadView() {
+        view = UIView(frame: .zero)
+        viewRespectsSystemMinimumLayoutMargins = false
+        view.layoutMargins = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
+        
+        view.backgroundColor = UIColor.red
+        
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let margins = view.layoutMarginsGuide
+        NSLayoutConstraint.activate([collectionView.leadingAnchor.constraint(equalTo: margins.leadingAnchor), collectionView.trailingAnchor.constraint(equalTo: margins.trailingAnchor)])
+        
+        let guide = view.safeAreaLayoutGuide
+        collectionView.topAnchor.constraintEqualToSystemSpacingBelow(guide.topAnchor, multiplier: 1.0).isActive = true
+        guide.bottomAnchor.constraintEqualToSystemSpacingBelow(collectionView.bottomAnchor, multiplier: 1.0).isActive = true
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +42,6 @@ class OffersViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
