a1c41fbb1f2457683e4f53d665385cee1b2efd7f
Richard Tackett <richtackett@hotmail.com>
2018-01-23 18:28:04 -0600

    Still working on cell layout

--- iBottaProject/OfferCollectionViewCell.swift
+++ iBottaProject/OfferCollectionViewCell.swift
@@ -40,20 +40,28 @@ private extension OfferCollectionViewCell {
         contentView.backgroundColor = UIColor.white
         contentView.translatesAutoresizingMaskIntoConstraints = false
         
-        backgroundCellView.translatesAutoresizingMaskIntoConstraints = false
         backgroundCellView.backgroundColor = UIColor.lightGray
         contentView.addSubview(backgroundCellView)
-        
+        backgroundCellView.translatesAutoresizingMaskIntoConstraints = false
         backgroundCellView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 24.0).isActive = true
         backgroundCellView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12.0).isActive = true
         contentView.trailingAnchor.constraint(equalTo: backgroundCellView.trailingAnchor, constant: 12.0).isActive = true
-        
-
         backgroundCellView.widthAnchor.constraint(equalToConstant: 150.0).isActive = true //Need to pass in the width
-        
         backgroundCellView.heightAnchor.constraint(equalTo: backgroundCellView.widthAnchor, multiplier: 1.0/1.5).isActive = true
         
-        contentView.bottomAnchor.constraint(equalTo: backgroundCellView.bottomAnchor, constant: 24.0).isActive = true //NEEDS TO CHANGE
+        amountLabel.text = "Amount"
+        contentView.addSubview(amountLabel)
+        amountLabel.translatesAutoresizingMaskIntoConstraints = false
+        amountLabel.topAnchor.constraint(equalTo: backgroundCellView.bottomAnchor, constant: 8.0).isActive = true
+        amountLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12.0).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: amountLabel.trailingAnchor, constant: 12.0).isActive = true
         
+        nameLabel.text = "Name"
+        contentView.addSubview(nameLabel)
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.topAnchor.constraint(equalTo: amountLabel.bottomAnchor, constant: 3.0).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 12.0).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 12.0).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 24.0).isActive = true //NEEDS TO CHANGE
     }
 }
